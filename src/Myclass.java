31d69c6650576b041fd8e9b6792167d099c9f951
Nilesh Kanti <kantinilesh2312@gmail.com>
2024-09-11 15:19:53 +0530

    Initial commit

---
+++ src/Myclass.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+class add {
+    int a;
+    int b;
+
+    add(int x, int y) {
+        a = x;
+        b = y;
+    }
+
+    void output() {
+        int c;
+        c = a + b;
+        System.out.println("Addition is" + c);
+    }
+}
+
+public static void main(String args[])
+{
+    int m,n;
+    Scanner sc=new Scanner(System.in);
+    System.out.pritln("Eneter first number");
+
+}
